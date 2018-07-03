96d2fde181b7978d0c228461801743e98c6d5e56
Nicholas <nclszelki@gmail.com>
2018-07-03 00:29:51 -0400

    Create robot.py

---
+++ robot.py
@@ -0,0 +1,19 @@
+import re
+import time
+import itchat
+from itchat.content import *
+
+@itchat.msg_register([TEXT],[PICTURE])
+def text_reply(msg):
+    reciever = itchat.search_friends(userName=msg['FromUserName']) # msg['FromUserName'] is encrypted
+    replyContent = "Received your message【%s】" % (msg['Type'])
+    if msg['Type'] == 'Text':
+        if re.search(r"happy",msg['Content']):
+            replyContent += "Yo! How are you!"
+            itchat.send('@img@%s' % 'bebaving-good.jpeg',toUserName=msg['FromUserName'])
+    itchat.send("friend:【%s（nickname：%s）】at：【%s】send messages: 【%s】" % (reciever['NickName'], reciever['RemarkName'], time.strftime('%Y-%m-%d %H:%M:%S',time.localtime()), msg['Text']),toUserName='filehelper')
+    itchat.send(replyContent,toUserName=msg['FromUserName'])
+
+
+itchat.auto_login(hotReload=True)
+itchat.run()
