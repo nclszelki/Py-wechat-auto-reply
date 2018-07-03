6c7eadda2ee94f80883c1df07d9de9da1e61a926
nicholas <nclszelki@gmail.com>
2018-07-03 13:31:02 -0400

    Updates changes

--- robot.py
+++ robot.py
@@ -5,12 +5,11 @@ from itchat.content import *
 
 @itchat.msg_register([TEXT],[PICTURE])
 def text_reply(msg):
-    reciever = itchat.search_friends(userName=msg['FromUserName']) # msg['FromUserName'] is encrypted
+    reciever = itchat.search_friends(userName=msg['filehelper']) # msg['FromUserName'] is encrypted
     replyContent = "Received your message【%s】" % (msg['Type'])
-    if msg['Type'] == 'Text':
-        if re.search(r"happy",msg['Content']):
-            replyContent += "Yo! How are you!"
-            itchat.send('@img@%s' % 'bebaving-good.jpeg',toUserName=msg['FromUserName'])
+    if msg['Type'] == 'Text' and re.search(r"auto-reply", msg['Content']):
+        replyContent += "Yo! How are you!"
+        itchat.send('@img@%s' % 'bebaving-good.jpeg',toUserName=msg['FromUserName'])
     itchat.send("friend:【%s（nickname：%s）】at：【%s】send messages: 【%s】" % (reciever['NickName'], reciever['RemarkName'], time.strftime('%Y-%m-%d %H:%M:%S',time.localtime()), msg['Text']),toUserName='filehelper')
     itchat.send(replyContent,toUserName=msg['FromUserName'])
 
